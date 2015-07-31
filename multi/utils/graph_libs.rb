2d0beaca4abfc14d0ff7800b369c26e4c185c0c1
Krzysztof Wróbel <djstrong@gmail.com>
2015-07-31 11:10:09 +0200

    mapping and classification together

---
+++ multi/utils/graph_libs.rb
@@ -0,0 +1,426 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+
+require 'bundler/setup'
+$:.unshift "../category-mapping/lib"
+require 'rlp/wiki'
+require 'progress'
+require 'csv'
+require 'slop'
+require 'set'
+require 'colors'
+require 'cycr'
+require 'mapping'
+require 'experiment_arguments_log/auto'
+require 'syntax'
+require 'nouns/nouns'
+require 'yajl'
+require 'auto_serializer'
+require 'dbm'
+
+
+
+class Node #article or category
+  attr_accessor :name, :relations, :information_nodes
+
+  def initialize(name)
+    @name=name
+    @relations = []
+    @information_nodes = []
+  end
+
+  def add_relation(relation)
+    relations << relation
+  end
+
+  def add_information(information_node)
+    @information_nodes << information_node
+  end
+
+  def all_informations
+    informations = @information_nodes.dup
+    @relations.each do |relation|
+      information_nodes.concat(relation.node.information_nodes)
+    end
+    return informations
+  end
+
+  def score_informations(name_service, information_node_name=nil)
+    @information_nodes.each do |information_node|
+      next if !information_node.assigned_type.nil?
+      information_node.candidates.each do |candidate|
+        next if !information_node_name.nil? && information_node.name==information_node_name
+
+        @information_nodes.each do |other_information_node|
+          next if information_node == other_information_node
+          score = candidate.calculate_score(other_information_node, name_service)
+          candidate.add_score(score)
+        end
+
+        self.relations.each do |relation|
+          score = Score.new
+          relation.node.information_nodes.each do |other_information_node|
+            score.add(candidate.calculate_score(other_information_node, name_service))
+          end
+          #if score < -1 then -1 - odlaczamy kategorie
+          if score.max_value < -1.0
+            # score.count = 1
+            score.max_value = -1.0
+          end
+          candidate.add_score(score)
+        end
+
+      end
+
+      #save best candidate for information_node
+      information_node.best_candidate = information_node.candidates.max_by { |candidate| candidate.score.value }
+    end
+  end
+end
+
+class Relation
+  attr_accessor :name, :node
+
+  def initialize(name, node)
+    @name = name
+    @node = node
+  end
+end
+
+class InformationNode #Namespace: fsX, headX
+  attr_accessor :name, :candidates, :assigned_type, :best_candidate
+
+  def initialize(name)
+    @name=name
+    @candidates=[]
+    @assigned_type = nil
+  end
+
+  def add_candidate(candidate)
+    @candidates << candidate
+  end
+
+
+end
+
+class Candidate
+  attr_accessor :cyc_id, :phrases, :score
+
+  def initialize(cyc_id, phrases)
+    @cyc_id = cyc_id
+    @phrases=phrases
+    @score = Score.new
+  end
+
+  def cyc_term(name_service)
+    name_service.find_by_id(@cyc_id)
+  end
+
+  def calculate_score(information_node, name_service)
+    score = Score.new
+
+
+
+    cyc = name_service.cyc
+
+    genls=igenls=nondisjoint=disjoint=isa=iisa=0
+
+    if !information_node.assigned_type.nil?
+      candidates = [Candidate.new(information_node.assigned_type, [])]
+    else
+      candidates = information_node.candidates
+    end
+    candidates.each do |candidate|
+      #TODO phrases rejection
+      if any_element_of_list_in_list(@phrases, candidate.phrases)
+        next
+      end
+      cyc_term2 = candidate.cyc_term(name_service)
+
+      #TODO isa
+      if cyc.with_any_mt{|c| c.genls?(cyc_term(name_service), cyc_term2)}
+        # p ['genls', cyc_term(name_service), cyc_term2, @phrases, candidate.phrases]
+        genls+=1
+      elsif cyc.with_any_mt{|c| c.genls?(cyc_term2, cyc_term(name_service)) }
+        # p ['igenls', cyc_term(name_service), cyc_term2, @phrases, candidate.phrases]
+        igenls+=1
+      elsif cyc.with_any_mt{|c| c.isa?(cyc_term(name_service), cyc_term2)} # cyc.isa?(cyc_term(name_service), cyc_term2)
+        # p ['isa', cyc_term(name_service), cyc_term2, @phrases, candidate.phrases]
+        isa+=1
+      elsif cyc.with_any_mt{|c| c.isa?(cyc_term2, cyc_term(name_service))} #cyc.isa?(cyc_term2, cyc_term(name_service))
+        # p ['iisa', cyc_term(name_service), cyc_term2, @phrases, candidate.phrases]
+        iisa+=1
+      elsif !cyc.with_any_mt{|c| c.disjoint_with?(cyc_term(name_service), cyc_term2)}
+        # p ['nondisjoint', cyc_term(name_service), cyc_term2, @phrases, candidate.phrases]
+        nondisjoint+=1
+      else
+        # p ['disjoint', cyc_term(name_service), cyc_term2, @phrases, candidate.phrases]
+        disjoint+=1
+      end
+
+      break if genls>0 && disjoint>0
+    end
+
+
+
+    scores = []
+    if genls>0
+      scores<<1.0
+    end
+    if igenls>0
+      scores<<0.7
+    end
+    if isa>0
+      scores<<0.5
+    end
+    if iisa>0
+      scores<<0.4
+    end
+    if nondisjoint>0
+      scores<<0.1
+    end
+    if disjoint>0
+      scores<<-1.0
+    end
+
+    if scores.size>0
+      score.count=1
+      score.min_value=scores.min
+      score.max_value=scores.max
+    end
+
+
+
+    return score
+  end
+
+  def add_score(score)
+    @score.add(score)
+  end
+end
+
+class Score
+  attr_accessor :max_value, :min_value, :count
+
+  def initialize
+    @max_value=0.0
+    @min_value=0.0
+    @count=0
+  end
+
+  def add(score)
+    @max_value += score.max_value
+    @min_value += score.min_value
+    @count += score.count
+  end
+
+  def value
+    @max_value*@count
+  end
+end
+
+def any_element_of_list_in_list(list1, list2) #TODO
+  list2.map!{|e| singularize(e.downcase)}
+  list2.flatten!
+  list2.uniq!
+  list1.map!{|e| singularize(e.downcase)}
+  list1.flatten!
+  list1.uniq!
+
+  list1.each do |element|
+    if list2.include? element
+      return true
+    end
+  end
+  return false
+end
+
+
+$nouns = Nouns.new
+
+def singularize(name)
+  names = [name]
+  singularized_heads = $nouns.singularize(name)
+  if not singularized_heads.nil?
+    names.concat singularized_heads
+  end
+  names
+end
+
+
+def build_information_nodes(dbm_structure, name)
+  information_nodes = []
+  dbm_structure.each do |namespace, namespace_candidates|
+    information_node = InformationNode.new(namespace)
+
+    if $assigned.include? ([name,namespace].to_s)
+      information_node.assigned_type = $assigned[[name,namespace].to_s]
+    else
+      namespace_candidates.each do |cyc_id, phrases|
+        information_node.add_candidate(Candidate.new(cyc_id, phrases))
+      end
+    end
+    information_nodes << information_node
+
+  end
+  information_nodes
+end
+
+def build_graph_for_category(category_name)
+  category = Category.find_by_name(category_name)
+  articles = category.concepts
+  parents = category.parents
+  children = category.children
+
+  category_node = Node.new(category_name)
+  ac = $category_candidates[category_name]
+  if not ac.nil?
+    ac = Yajl::Parser.parse(ac)
+
+
+    build_information_nodes(ac, 'Category:'+category_name).each do |information|
+      category_node.add_information(information)
+    end
+  end
+
+  articles.each do |article|
+    next if !article.regular?
+    name = article.name
+    ac = $article_candidates[name]
+    next if ac.nil?
+    ac = Yajl::Parser.parse(ac)
+    node = Node.new(name)
+    category_node.add_relation(Relation.new('article', node))
+
+    build_information_nodes(ac, name).each do |information|
+      node.add_information(information)
+    end
+  end
+
+  parents.each do |category|
+    next if !category.regular? || !category.plural?
+    name = category.name
+    ac = $category_candidates[name]
+    next if ac.nil?
+    ac = Yajl::Parser.parse(ac)
+    node = Node.new(name)
+    category_node.add_relation(Relation.new('parent', node))
+
+    build_information_nodes(ac, 'Category:'+name).each do |information|
+      node.add_information(information)
+    end
+  end
+
+  children.each do |category|
+    next if !category.regular? || !category.plural?
+    name = category.name
+    ac = $category_candidates[name]
+    next if ac.nil?
+    ac = Yajl::Parser.parse(ac)
+    node = Node.new(name)
+    category_node.add_relation(Relation.new('child', node))
+
+    build_information_nodes(ac, 'Category:'+name).each do |information|
+      node.add_information(information)
+    end
+  end
+
+  return category_node
+end
+
+def build_graph_for_article(article_name)
+  concept = Concept.find_by_name(article_name)
+
+  categories = concept.categories
+
+
+  article_node = Node.new(article_name)
+  ac = $article_candidates[article_name]
+  if not ac.nil?
+    ac = Yajl::Parser.parse(ac)
+
+
+    build_information_nodes(ac, article_name).each do |information|
+      article_node.add_information(information)
+    end
+  end
+
+
+
+  categories.each do |category|
+    next if !category.regular? || !category.plural?
+    name = category.name
+    ac = $category_candidates[name]
+    next if ac.nil?
+    ac = Yajl::Parser.parse(ac)
+    node = Node.new(name)
+    article_node.add_relation(Relation.new('parent', node))
+
+    build_information_nodes(ac, 'Category:'+name).each do |information|
+      node.add_information(information)
+    end
+  end
+
+
+
+  return article_node
+end
+
+
+def get_connected_names_for_article(article_name)
+  connected = []
+  concept = Concept.find_by_name(article_name)
+
+  categories = concept.categories
+
+
+
+
+  categories.each do |category|
+    next if !category.regular? || !category.plural?
+    name = category.name
+    ac = $category_candidates[name]
+    next if ac.nil?
+    connected << 'Category:'+name
+  end
+
+
+
+  return connected
+end
+
+def get_connected_names_for_category(category_name)
+  connected = []
+  category = Category.find_by_name(category_name)
+  articles = category.concepts
+  parents = category.parents
+  children = category.children
+
+
+  articles.each do |article|
+    next if !article.regular?
+    name = article.name
+    ac = $article_candidates[name]
+    next if ac.nil?
+    connected << name
+  end
+
+  parents.each do |category|
+    next if !category.regular? || !category.plural?
+    name = category.name
+    ac = $category_candidates[name]
+    next if ac.nil?
+    connected << 'Category:'+name
+  end
+
+  children.each do |category|
+    next if !category.regular? || !category.plural?
+    name = category.name
+    ac = $category_candidates[name]
+    next if ac.nil?
+    connected << 'Category:'+name
+  end
+
+  return connected
+end
---
+++ multi/utils/queue.rb
@@ -0,0 +1,152 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+
+require 'bundler/setup'
+$:.unshift "../category-mapping/lib"
+require 'rlp/wiki'
+require 'progress'
+require 'csv'
+require 'slop'
+require 'set'
+require 'colors'
+require 'cycr'
+require 'mapping'
+require 'experiment_arguments_log/auto'
+require 'syntax'
+require 'nouns/nouns'
+require 'yajl'
+require 'auto_serializer'
+require 'dbm'
+require 'pqueue'
+require 'priority_queue'
+require 'fc'
+require './utils/graph_libs.rb'
+
+options = Slop.new do
+  banner "#{$PROGRAM_NAME} -m reference.csv -i classification.csv -s [s,a,n,w,ma,cm]\n" +
+             "Generates all phrases for denotation mapping."
+
+  # on :m=, :reference, 'Reference classification', required: true
+  # on :i=, :classification, 'Automatic (verified) classification', required: true
+  # on :s=, :score, 'Scoring measure: s - simple (micro), a - aprosio, n - aprosio normalized, ma - macro, w - weighted, cm - confusion matrix', required: true
+  on :h=, :host, "Cyc host", default: 'localhost'
+  on :p=, :port, "Cyc port", as: Integer, default: 3601
+  on :c=, :"category-filters", "Filters for categories: c - collection, s - most specific,\n" +
+            "n - noun, r - rewrite of, l - lower case, f - function, c|i - collection or individual, b - black list", default: 'c:r:f:l:d'
+  on :g=, :"genus-filters", "Filters for genus proximum types: (as above)", default: 'c:r:f:l:d'
+end
+
+begin
+  options.parse
+rescue => ex
+  puts ex
+  puts options
+  exit
+end
+
+cyc = Cyc::Client.new(cache: true, host: options[:host], port: options[:port])
+name_service = Mapping::Service::CycNameService.new(cyc)
+
+include Rlp::Wiki
+Database.instance.open_database(options[:database] || '../../en-2013/')
+
+$category_candidates = DBM.open('category_candidate')
+$article_candidates = DBM.open('joined')
+
+
+#TODO infer categories, e.g. Boston Bruins players
+
+recalculate = DBM.open('recalcualte')
+$assigned = DBM.open('assigned3')
+q = FastContainers::PriorityQueue.new(:max)
+
+CSV.open('results.csv') do |input|
+  input.with_progress do |row|
+    name, information_name, cyc_id, cyc_name, max_value, count, value = row
+    # pq.push([value, row])
+    q.push([name, information_name, cyc_id], value.to_f)
+  end
+end
+
+CSV.open('results2.csv') do |input|
+  input.with_progress do |row|
+    name, information_name, cyc_id, cyc_name, max_value, count, value = row
+    # pq.push([value, row])
+    q.push([name, information_name, cyc_id], value.to_f)
+  end
+end
+
+CSV.open('results_multi2.csv', 'w') do |output|
+  Progress.start(q.size)
+  while !q.empty? do
+
+    name, information_name, cyc_id = q.top
+    value = q.top_key
+    q.pop
+
+    #sprawdz czy trzeba przeliczyc
+    if recalculate.include?([name, information_name].to_s)
+      # recalculate and next
+      if name.start_with?('Category:')
+        node=build_graph_for_category(name[9..-1])
+      else
+        node=build_graph_for_article(name)
+      end
+      node.score_informations(name_service, information_name)
+
+      p ['recalculated', name]
+      node.information_nodes.each do |information_node|
+        next if information_name!=information_node.name
+        # cyc_name = information_node.best_candidate.cyc_term(name_service).to_ruby.to_s
+        # output << ['Category:'+node.name, information_node.name, information_node.best_candidate.cyc_id, cyc_name, information_node.best_candidate.score.max_value,  information_node.best_candidate.score.count,  information_node.best_candidate.score.value]
+        if !information_node.assigned_type.nil?
+          next
+        end
+        q.push([name, information_node.name, information_node.best_candidate.cyc_id], information_node.best_candidate.score.value)
+        recalculate.delete([name, information_node.name].to_s) #TODO zapisywac aktualne value dla wszystkich
+      end
+
+      #usun z recalc
+      next
+    end
+
+    Progress.step
+    $assigned[[name, information_name].to_s]= cyc_id
+    #TODO mark nodes for recalc
+    output << [name, information_name, cyc_id, value]
+
+    if name.start_with?('Category:')
+      node=build_graph_for_category(name[9..-1])
+    else
+      node=build_graph_for_article(name)
+    end
+    node.relations.each do |relation|
+      relation.node.information_nodes.each do |information_node|
+        next if !information_node.assigned_type.nil?
+        recalculate[[relation.node.name, information_node.name].to_s]='X'
+      end
+    end
+  end
+end
+
+
+#TODO category names prefixed
+#TODO parallel
+#TODO jak ujednolicic score dla kategorii i artykulow?
+#TODO jezeli min nie potrzebne to nieliczyc - znacznie przypspieszy
+
+
+# posortowac wezly
+# przypisac i przekalkulowac zalezne
+# PRZYPISANIE może tylko zmneijszać wynik, wiec zamiast kalkulacji oznaczyc jako do przeliczenia
+#napotykając węzeł do przeliczenia trzeba zaktualizować i wrzucić w odpowiednie meijsce kolejki
+
+#posortowac external
+#zrobic kolejke na aktualizacje
+#czytac z powyzszych dwoch zrodel
+#pominac mneijsze od 0?
+
+# wydaje sie, ze przypisanie typuob cos psuj
+# Category:British diplomats,name,Mx8Ngx4rwEcGC5wpEbGdrcN5Y29ycB4rvVj_jpwpEbGdrcN5Y29ycB4rv5VvH5wpEbGdrcN5Y29ycA,1242864.000000005
+# Category:British diplomats,head0,Mx8Ngx4rwEcGC5wpEbGdrcN5Y29ycB4rvVj_jpwpEbGdrcN5Y29ycB4rv5VvH5wpEbGdrcN5Y29ycA,1240587.800000005
